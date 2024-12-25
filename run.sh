91c5b8e4140b4b626c4cee1ed3285fa3995df4c4
Alain Dutech <alain.dutech@loria.fr>
2024-12-25 10:09:53 +0100

    ADD run command (for Main24)

---
+++ run.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+yead_day=""
+arg_interact="0"
+arg_nobuild="0"
+
+cmd="cabal new-" # run ou repl
+
+while true ; do
+    case "$1" in
+	-i|--interact)
+	    arg_interact="1"; shift 1;;
+    -n|--no_build)
+        arg_nobuild="1"; shift 1;;
+	-h|--help)
+	    echo "usage $0 [-i/--interact] [-n/--no_build] yy_dd"
+	    exit;;
+	*)
+        # need one file name
+        if [ "$#" -lt 1 ];
+        then echo "illegal number of parameters"
+             echo "Usage : run [-i] yy_dd"
+             exit;
+        fi
+        year_day=$1;
+	    break;;
+    esac
+done
+
+year=${year_day:0:2}
+advent_name="src/advent"$year_day".hs";
+link_name="app/Main"$year".hs"
+target_name="Advent"$year
+
+if [ "$arg_interact" == "1" ]; then
+    cmd=$cmd"repl "$target_name
+else
+    cmd=$cmd"run "$target_name
+fi
+
+if test -f $advent_name; then
+    echo $advent_name" EXISTS"
+    echo "rm -f "$link_name
+    rm -f $link_name
+    echo "ln -s ../"$advent_name" "$link_name
+    ln -s "../"$advent_name $link_name
+    if [ "$arg_nobuild" == "0" ]; then
+        echo "cabal new-clean"
+        cabal new-clean
+        echo "cabal new-build $link_name"
+        cabal new-build $link_name
+    fi
+    echo $cmd
+    $cmd
+else
+    echo $advent_name" DOES NOT exist"
+    exit;
+fi
