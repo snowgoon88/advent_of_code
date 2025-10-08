62a2329c0465e906bacbba57fd8f0be6d18d40a1
Alain Dutech <alain.dutech@loria.fr>
2025-10-08 23:08:10 +0200

    UPD MyUtils with untilM, nTimesM

--- src/MyUtils.hs
+++ src/MyUtils.hs
@@ -31,3 +31,19 @@ cpred x | x == minBound = maxBound
 -- example applyN (+ 2) 10 1 |-> 21
 applyN :: (a -> a) -> Int -> a -> a
 applyN f n = \item -> iterate f item !! n
+
+-- | Apply 'n' times a Monadic Action
+-- example nTimesM monadAction 10 startValue
+nTimesM :: (Ord t, Num t, Monad m) => (a -> m a) -> t -> a -> m a
+nTimesM f n item
+  | n > 0 = f item >>= nTimesM f (n-1)
+  | otherwise = return item
+
+-- | Apply a Monadic Action until a condition is True
+-- example untilM isValidPath growPath startPath
+-- see advent21_12.hs
+untilM :: Monad m => (a -> Bool) -> (a -> m a) -> a -> m a
+untilM cond f item = do
+  let finished = cond item
+  if finished then return item
+              else f item >>= untilM cond f
